c7c30f5412d35663288b54dfad83459a028e51db
Rami Mansieh <rmansieh@gmail.com>
2022-05-12 03:20:22 -0400

    complete create button OnClick functionality, complete edit and title button onClick functionality using row input values (to be replaced with api call values)

--- dvd.js
+++ dvd.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
             type: 'GET',
             url: ' http://dvd-library.us-east-1.elasticbeanstalk.com/dvds',
             success: function (dvds) {
-                console.log(dvds);
                 $('#tableContainer').show();
                 $('#methodHeader').hide();
                 $('#formContainer').hide();
@@ -21,11 +20,23 @@ $(document).ready(function () {
                     e.preventDefault();
                     var row = $(this).closest('tr');
                     var title = $(this).text();
-                    var releaseYear = row.find(".releaseYear").text();
-                    var director = row.find(".director").text();
-                    var rating = row.find(".rating").text();
-                    console.log(title + " " + releaseYear + " " + director + " " + rating);
+                    var releaseYear = row.find('.releaseYear').text();
+                    var director = row.find('.director').text();
+                    var rating = row.find('.rating').text();
+                    var notes = '';
+                    
                 });
+                $('.editLink').on('click', function(e){
+                    e.preventDefault();
+                    var row = $(this).closest('tr');
+                    var title = row.find('.title').text();
+                    var releaseYear = row.find('.releaseYear').text();
+                    var director = row.find('.director').text();
+                    var rating = row.find('.rating').text();
+                    var notes = '';
+                    showEditForm(title, releaseYear, director, rating, notes);
+                });
+
 
             },
             error: function () {
@@ -34,5 +45,49 @@ $(document).ready(function () {
 
         });
     }
+
+    function clearFormValues (){
+        $('#titleInput').val('');
+        $('#releaseYear').val('');
+        $('#directorInput').val('');
+        $('#rating option').removeAttr('selected');
+        $('#rating option[value="G"]').attr('selected', true); 
+    }
+
+    function showEditForm(title, releaseYear, director, rating, notes) {
+        setFormValues(title, releaseYear, director, rating, notes);
+        $('#headerTitle').text('Edit DVD: ' + title);
+        $('#tableContainer').hide();
+        $('#methodHeader').show();
+        $('#formContainer').show();
+    }
+
+    function setFormValues(title, releaseYear, director, rating, notes){
+        
+        
+        $('#titleInput').val(title);
+        $('#releaseYear').val(releaseYear);
+        $('#directorInput').val(director);
+        $('#rating option').removeAttr('selected');
+        $('#rating option[value=' + rating + ']').attr('selected', true);
+
+    }
+
+    function showCreateForm () {
+        $('#tableContainer').hide();
+        $('#headerTitle').text('Create DVD');
+        $('#methodHeader').show();
+        clearFormValues();
+        $('#formContainer').show();
+    }
+
+    $("#createDVD").on('click', function(){
+        showCreateForm();
+    });
+
+    $("#formBackBtn").on('click', function() {
+        getDvds();
+    });
     getDvds();
+    
 });
